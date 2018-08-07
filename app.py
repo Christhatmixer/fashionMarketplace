445df36533e4d1b2b3a9e05384042674058d415f
Chris Farley <cfarley95@gmail.com>
2018-08-07 03:28:33 -0400

    test

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def getUserInfo():
     print(data)
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT 1 FROM users WHERE userID = %s"
+            sql = "SELECT 1 FROM users WHERE users.userid = %s"
             cursor.execute(sql, (data["userID"]))
             result = cursor.fetchall()
             print(result)
